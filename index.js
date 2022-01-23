d0cb3822a023ab51c3d02c5ae930da1972f40f99
= <ejkim93@sk.com>
2022-01-23 17:01:49 +0900

    test

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ if (savedUsername === null) {
 //background
 //const fs = require("fs");
 readdir("./img", (err, files) => {
-  document.body.style.backgroundImage = `url('./img/${Math.floor(
+  document.body.style.backgroundImage = `url('/img/${Math.floor(
     Math.random() * files.length
   )}.jpg')`;
   document.body.style.backgroundSize = "cover";
